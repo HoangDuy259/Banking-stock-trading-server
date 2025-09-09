df82ce28f3404a822dc450a53e1d7f4196761348
Huynh Nhut Duong <84580739+hnduong0711@users.noreply.github.com>
2025-09-09 16:44:08 +0700

    Merge pull request #3 from HoangDuy259/duong
    Func: Using security to check token when user do sth

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,74 +1,78 @@
-//package com.example.demo.controller;
-//
-//import com.example.demo.dto.request.RefreshTokenRequest;
-//import com.example.demo.dto.request.RegisterRequest;
-//import com.example.demo.exception.AppException;
-//import com.example.demo.service.UserService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/auth/users")
-//@RequiredArgsConstructor
-//public class UserController {
-//    private final UserService userService;
-//    private final JwtUtils jwtUtils;
-//
-//    @PostMapping("/register")
-//    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
-//        return ResponseEntity.ok(userService.register(request));
-//    }
-//
-//    @PostMapping("/login")
-//    public ResponseEntity<UserResponse> login(@RequestBody LoginRequest request) {
-//        UserResponse response = userService.login(request);
-//        return ResponseEntity.ok(response);
-//    }
-//
-//    @GetMapping("/info")
-//    public ResponseEntity<TokenResponse> getUserInfo(@RequestHeader("Authorization") String token) {
-//        // Xóa "Bearer " từ token
-//        if (token != null && token.startsWith("Bearer ")) {
-//            token = token.substring(7);
-//        }
-//
-//        // Lấy username từ token
-//        String username = JwtUtils.getUsernameFromToken(token);
-//
-//        // Lấy thông tin user
-//        TokenResponse userInfo = userService.getUserInfo(username);
-//        return ResponseEntity.ok(userInfo);
-//    }
-//
-////    Duong code ne
-////    cấp lại access token khi hết hạn bằng refresh token đang có
-//    @PostMapping("/refresh")
-//    public ResponseEntity<RefreshTokenResponse> refreshToken(@RequestBody RefreshTokenRequest request) {
-//        String refreshToken = request.getRefreshToken();
-//
-////        if (!jwtUtils.validateToken(refreshToken)) {
-////            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-////        }
-//
-//        if (!jwtUtils.validateToken(refreshToken, "refresh")) {
-//            throw new AppException.InvalidToken("Invalid refresh token");
-//        }
-//
-//        String username = jwtUtils.getUsernameFromToken(refreshToken);
-//
-//        UserDetails userDetails = userService.loadUserByUsername(username);
-//
-//        // tạo access token mới
-//        String newAccessToken = jwtUtils.generateToken(userDetails.getUsername());
-//
-//        RefreshTokenResponse response = new RefreshTokenResponse(
-//                newAccessToken,
-//                jwtUtils.getAccessTokenExpiration()
-//        );
-//
-//        return ResponseEntity.ok(response);
-//    }
-//
-//}
+package com.example.demo.controller;
+
+import com.example.demo.dto.identity.TokenExchangeResponse;
+import com.example.demo.dto.request.auth.LoginRequest;
+import com.example.demo.dto.request.auth.UserRegisterRequest;
+import com.example.demo.dto.response.ApiResponse;
+import com.example.demo.dto.response.user.UserResponse;
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.metadata.HsqlTableMetaDataProvider;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.security.sasl.AuthenticationException;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class UserController {
+    UserService userService;
+    @GetMapping("/myInfo")
+    public ResponseEntity<ApiResponse<UserResponse>> getMyInfo(){
+        try {
+            return ResponseEntity.status(CREATED).body(
+                    new ApiResponse<UserResponse>("Thành công", userService.getMyInfo()));
+        } catch (RuntimeException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ApiResponse<>(e.getMessage(), null));
+        }
+
+    }
+
+    @GetMapping("/getAllUsers")
+    public ResponseEntity<ApiResponse<UserResponse>> getAllUsers(){
+        try {
+            return ResponseEntity.status(CREATED).body(
+                    new ApiResponse<UserResponse>("Thành công", userService.getMyInfo()));
+        } catch (RuntimeException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ApiResponse<>(e.getMessage(), null));
+        }
+
+    }
+    @GetMapping("/update")
+    public ResponseEntity<ApiResponse<UserResponse>> updateMyInfo(){
+        try {
+            return ResponseEntity.status(CREATED).body(
+                    new ApiResponse<UserResponse>("Thành công", userService.getMyInfo()));
+        } catch (RuntimeException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ApiResponse<>(e.getMessage(), null));
+        }
+
+    }
+
+    @GetMapping("/delete")
+    public ResponseEntity<ApiResponse<UserResponse>> deleteUser(){
+        try {
+            return ResponseEntity.status(CREATED).body(
+                    new ApiResponse<UserResponse>("Thành công", userService.getMyInfo()));
+        } catch (RuntimeException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ApiResponse<>(e.getMessage(), null));
+        }
+
+    }
+
+}
--- src/main/java/com/example/demo/dto/request/auth/LoginRequest.java
+++ src/main/java/com/example/demo/dto/request/auth/LoginRequest.java
@@ -1,9 +1,21 @@
 package com.example.demo.dto.request.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.experimental.FieldDefaults;
 
 @Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class LoginRequest {
-    private String username;
-    private String password;
+     @NotBlank(message = "Email is REQUIRED")
+     @Email(message = "Email is invalid")
+     @Size(max = 100, message = "Email limit is 100 characters")
+     String email;
+
+     @NotBlank(message = "Password is REQUIRED")
+     @Size(max = 50, message = "Password limit is 50 characters")
+     String password;
 }
